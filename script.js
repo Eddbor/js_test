5249e79468265b3305d91a32e0f585aa11ce39ed
EddBor <3ddbor@gmail.com>
2024-04-07 16:49:51 +0100

    Turn promt into buttons
    Turned the inicial prompt into buttons. Still have gun as a secret option. Made it look better with css.
    It resets to 0 after reaching 5 points and pressing 1 of the buttons.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let computerMove
 let playerMove
 let playerPoints = 0
 let computerPoints = 0
+resultstext.textContent = `Chose a weapon.`  
 
 //The bots choice small chance of getting a gun wich is a automatic win or tie and also makes it a 1/3 chance of getting the other 3 options of a rock/papper/scissors game.
 function getComputerChoice() {
@@ -19,62 +20,123 @@ function getComputerChoice() {
     }
 }
 
-//This is the prompt that i need for the player to choose his weapon makes his answer lower case so he can type it however he wants it will read it no matter what.
-//If the player does type anything else it will tell the player to type another one. Gun is a option but we don't tell them that.
-function getPlayerChoice(){
-    let playerChoice = prompt('Chose your MEDIEVAL weapon to kill your clone!', '(Bow, Shield, Sword)')
-    let convPlayerMove = playerChoice.toLowerCase()
-    if (convPlayerMove == 'bow'){
-        playerMove = 'Bow'
-    }else if(convPlayerMove == 'shield'){
-        playerMove = 'Shield'
-    }else if(convPlayerMove == 'sword'){
-        playerMove = 'Sword'
-    }else if(convPlayerMove == 'gun'){
-        playerMove = 'Gun'
-        alert('Where did you find that?')
-    }else{
-        alert("You look around and you don't find that weapon... Try to look for another one.")
-        return(getPlayerChoice())
+const getPlayerChoice = document.querySelectorAll('button');
+for(let i = 0; i < getPlayerChoice.length; i++){
+    getPlayerChoice[i].addEventListener('click', () => {
+            if(playerPoints == 5 || computerPoints == 5){
+                computerPoints = 0
+                playerPoints = 0
+                winner.textContent = ' '
+                computerP.textContent = `${computerPoints}`
+                playerP.textContent = `${playerPoints}`
+            }else if(getPlayerChoice[i].classList.contains('bow')){
+                playerMove = 'Bow'
+                resultstext.textContent = `${playRound()}`
+            }else if(getPlayerChoice[i].classList.contains('shield')){
+                playerMove = 'Shield'
+                resultstext.textContent = `${playRound()}`
+            }else if(getPlayerChoice[i].classList.contains('sword')){
+                playerMove = 'Sword'
+                resultstext.textContent = `${playRound()}`
+            }
+        })
     }
-}
+const secret = document.querySelector('#secret')
+secret.addEventListener('click', () => {
+    playerMove = 'Gun'
+    alert('Where did you find that?')
+    resultstext.textContent = `${playRound()}`
+})
 
 //these are the many outcomes that can come with a round including an extra one for gun vs gun, player using gun, and bot using gun.
 function playRound(){
     getComputerChoice()
-    getPlayerChoice()
     if (computerMove == 'Gun' && playerMove == 'Gun'){
         return("Ok... thats proof that he is indeed your clone... you both are cowards. After missing all of your shots you both try to knock eachother out and manage to do so. It's a Tie... a pitifull Tie")
     }else if (computerMove == playerMove){
         return(`You both had ${computerMove}s and killed eachother. It's a Tie`)
     }else if(computerMove == 'Gun'){
+        if(computerPoints == 4){
+            computerPoints++
+            computerP.textContent = `${computerPoints}`
+            if(playerPoints == 1){
+                winner.textContent = `Your clone won the war. The clone died ${playerPoints} times. You died ${computerPoints} time`
+            }else{
+                winner.textContent = `Your clone won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`
+            }
+            return(`Somehow your clone managed to find a Gun. And it seems a ${playerMove} couldn't quite do it. You got shot square in the head.`)
+        }else{
         computerPoints++
+        computerP.textContent = `${computerPoints}`
         return(`Somehow your clone managed to find a Gun. And it seems a ${playerMove} couldn't quite do it. You got shot square in the head.`)
+        }
     }else if(computerMove == 'Bow' && playerMove == 'Sword' || computerMove == 'Sword' && playerMove == 'Shield' || computerMove == 'Shield' && playerMove == 'Bow'){
+        if(computerPoints == 4){
+            computerPoints++
+            computerP.textContent = `${computerPoints}`
+            if(playerPoints == 1){
+                winner.textContent = `Your clone won the war. The clone died ${playerPoints} times. You died ${computerPoints} time`
+            }else{
+                winner.textContent = `Your clone won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`
+            }
+            return(`Your clone had a ${computerMove} wich gave him the upper hand against your ${playerMove} and won the match. You died.`)
+        }else{
         computerPoints++
+        computerP.textContent = `${computerPoints}`
         return(`Your clone had a ${computerMove} wich gave him the upper hand against your ${playerMove} and won the match. You died.`)
+        }
     }else if(playerMove == 'Gun'){
+        if(playerPoints == 4){
+            playerPoints++
+            playerP.textContent = `${playerPoints}`
+            if(computerPoints == 1){
+                winner.textContent = `You won the war. The clone died ${playerPoints} times. You died ${computerPoints} time`
+            }else{
+                winner.textContent = `You won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`
+            }
+            return(`You (somehow) brought a Gun to a Medieval match... you have no honor. After missing half the shots you mannaged to kill you clone. Hope you are happy.`)
+        }else{
         playerPoints++
-        return('You (somehow) brought a Gun to a Medieval match... you have no honor. After missing half the shots you mannaged to kill you clone. Hope you are happy.')
-    }else {
+        playerP.textContent = `${playerPoints}`
+        return(`You (somehow) brought a Gun to a Medieval match... you have no honor. After missing half the shots you mannaged to kill you clone. Hope you are happy.`)
+        }
+    }else{
+        if(playerPoints == 4){
+            playerPoints++
+            playerP.textContent = `${playerPoints}`
+            if(computerPoints == 1){
+                winner.textContent = `You won the war. The clone died ${playerPoints} times. You died ${computerPoints} time`
+            }else{
+                winner.textContent = `You won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`
+            }
+            return(`You won fair and square with your ${playerMove} against your clone's ${computerMove}. The real one stands proud in the center of the arena. Good job.`)
+        }else{
         playerPoints++
+        playerP.textContent = `${playerPoints}`
         return(`You won fair and square with your ${playerMove} against your clone's ${computerMove}. The real one stands proud in the center of the arena. Good job.`)
+        }
     }
 }
 
-//This is the function that repeats the round 5 times. Pondering if it should be a best of 5 or not.
-function playGame(){
-for(let numbOfRounds = 0; numbOfRounds < 5; numbOfRounds++){
-    console.log(playRound())
-}
-if (computerPoints > playerPoints){
-    alert(`Your clone won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`)
-}else if (computerPoints < playerPoints){
-    alert(`You won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`)
-}else{
-    alert(`You both lay dead. The clone died ${playerPoints} times. You died ${computerPoints} times`)
-}
+const playerP = document.querySelector('#playerP')
+playerP.textContent = `${playerPoints}`
 
-}
+const computerP = document.querySelector('#computerP')
+computerP.textContent = `${computerPoints}`
 
-playGame()
+const winner = document.querySelector('#winner')
+
+//function playGame(){
+//for(let numbOfRounds = 0; numbOfRounds < 5; numbOfRounds++){
+//    console.log(playRound())
+//}
+//if (computerPoints > playerPoints){
+//    alert(`Your clone won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`)
+//}else if (computerPoints < playerPoints){
+//    alert(`You won the war. The clone died ${playerPoints} times. You died ${computerPoints} times`)
+//}else{
+//    alert(`You both lay dead. The clone died ${playerPoints} times. You died ${computerPoints} times`)
+//}
+//}
+
+
